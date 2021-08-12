db2a4031da6cc2f5e97a9ae112fd2bfd2fc47c24
joseph58080 <joseph58080@gmail.com>
2021-08-12 15:06:14 +0900

    [최우영] - 8/12 예제

---
+++ src/main/java/com/majon/spring/lesson01/Ex02Controller.java
@@ -0,0 +1,16 @@
+package com.majon.spring.lesson01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Ex02Controller {
+
+		// 요청 url : http://localhost:8080/lesson01/ex02/1
+		@RequestMapping("/lesson01/ex02/1")
+			public String view() {
+			
+			
+			return "/lesson01/ex02";
+		}
+}
