16163bd93e34afebe5170253e013920e2ef53d0a
joseph58080 <joseph58080@gmail.com>
2021-08-19 15:57:53 +0900

    [최우영] - jsp 예제

---
+++ src/main/java/com/majon/spring/lesson04/bo/NewUserBO.java
@@ -0,0 +1,25 @@
+package com.majon.spring.lesson04.bo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.majon.spring.lesson04.dao.NewUserDAO;
+import com.majon.spring.lesson04.model.NewUser;
+
+@Service
+public class NewUserBO {
+	
+	@Autowired
+	private NewUserDAO newUserDAO;
+	
+	public int addUser(String name, String yyyymmdd, String introduce, String email) {
+		
+		return newUserDAO.insertUser(name, yyyymmdd, introduce, email);
+		
+	}
+	
+	public NewUser getLastUser() {
+		return newUserDAO.selectLastUser();
+	}
+
+}
