f51c94e717d5919f3c0129efae9cc0422f3c4657
joseph58080 <joseph58080@gmail.com>
2021-08-23 16:06:35 +0900

    [최우영]  - JSTL Core Library 예제

---
+++ src/main/java/com/majon/spring/lesson05/Lesson05Controller.java
@@ -0,0 +1,57 @@
+package com.majon.spring.lesson05;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/lesson05")
+public class Lesson05Controller {
+
+	@GetMapping("/ex01")
+	public String ex01() {
+		return "/lesson05/ex01";
+	}
+	
+	@GetMapping("/ex02")
+	public String ex02(Model model) {
+		
+		// 과일 이름을 저장하고 있는 리스트
+		
+		List<String>fruits = new ArrayList<>();
+		fruits.add("수박");
+		fruits.add("딸기");
+		fruits.add("포도");
+		
+		model.addAttribute("fruits", fruits);
+		
+		// List<Map>
+		// [ {name:"김인규", age:28, hobby:"댄스"}, ....]
+		
+		List<Map<String,Object>>users = new ArrayList<>();
+		
+		Map<String,Object> map = new HashMap<>();
+		map.put("name", "김인규");
+		map.put("age", 28);
+		map.put("hobby", "댄스");
+		users.add(map);
+		
+		map = new HashMap<>();
+		map.put("name", "김바다");
+		map.put("age", 3);
+		map.put("hobby", "사냥하기");
+		users.add(map);
+		
+		model.addAttribute("users", users);
+		
+		return "lesson05/ex02";
+	}
+	
+	
+}
